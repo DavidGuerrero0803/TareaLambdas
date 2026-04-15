1b841a65a77449eb782c09e2d789c11cdee7efde
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-04-15 13:29:43 -0700

    Creado el primer método estático + primera prueba en la clase Main (funciona como se debe).

--- src/EjerciciosStaticLambdas.java
+++ src/EjerciciosStaticLambdas.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class EjerciciosStaticLambdas {
+
+    //=======================================================================
+    //                          MÉTODOS ESTÁTICOS
+    //=======================================================================
+
+    public static void multiplicadorArrayList(ArrayList<Integer> numeros, int multiplicador) {
+        numeros.replaceAll(n -> n * 3);
+        System.out.println("=== MULTIPLICADOR ===");
+        System.out.println(numeros);
+    }
 }
