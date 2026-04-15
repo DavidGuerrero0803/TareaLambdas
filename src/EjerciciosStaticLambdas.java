d60de8dbcf0c73d6875c428141e539a709612dd6
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-04-15 13:48:10 -0700

    Creado el segundo método estático que funciona como filtro selectivo con cadenas de texto (se ha verificado su funcionamiento en pruebas).

--- src/EjerciciosStaticLambdas.java
+++ src/EjerciciosStaticLambdas.java
@@ -6,9 +6,28 @@ public class EjerciciosStaticLambdas {
     //                          MÉTODOS ESTÁTICOS
     //=======================================================================
 
+    /**
+     * Este metodo estático pide un ArrayList de números y un factor de multiplicación,
+     * el resultado de las multiplicaciones se mostrarán en pantalla.
+     * @param numeros (el ArrayList con los números).
+     * @param multiplicador (el factor por el que los vas a multiplicar).
+     */
     public static void multiplicadorArrayList(ArrayList<Integer> numeros, int multiplicador) {
         numeros.replaceAll(n -> n * 3);
         System.out.println("=== MULTIPLICADOR ===");
         System.out.println(numeros);
+    }
+
+    /**
+     * Este metodo estático pide una ArrayList de cadenas, un caracter para eliminar palabras
+     * y una cantidad con la cual eliminar una cadena de n longitud.
+     * @param cadena (el ArrayList que contiene cadenas de textos).
+     * @param letra (el caracter con el cual borrar palabras que empiecen con él).
+     * @param n (longitud de las palabras que se desean eliminar).
+     */
+    public static void filtradorSelectivo(ArrayList<String> cadena, char letra, int n) {
+        cadena.removeIf(c -> c.startsWith(String.valueOf(letra)) || c.length() < n);
+        System.out.println("=== FILTRO SELECTIVO ===");
+        System.out.println(cadena);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        // EJERCICIO 1.
         ArrayList<Integer> numeros = new ArrayList<>();
         numeros.add(10);
         numeros.add(20);
 
         EjerciciosStaticLambdas.multiplicadorArrayList(numeros, 2);
+
+        // EJERCICIO 2.
+        ArrayList<String> cadena = new ArrayList<>();
+        cadena.add("Dulce");
+        cadena.add("Salado");
+        cadena.add("Amargo");
+        cadena.add("Agrio");
+
+        EjerciciosStaticLambdas.filtradorSelectivo(cadena, 'A', 6);
     }
 }
