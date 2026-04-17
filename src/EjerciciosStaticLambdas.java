d093060c608bcc10f1825c95c60e3433625c6d72
David <mario.guerrero.marquez@uabc.edu.mx>
2026-04-17 16:26:50 -0700

    Añadidos comentarios descriptivos a los 10 métodos estáticos para mayor comprensión.

--- src/EjerciciosStaticLambdas.java
+++ src/EjerciciosStaticLambdas.java
@@ -14,7 +14,8 @@ public class EjerciciosStaticLambdas {
      * @param multiplicador (el factor por el que los vas a multiplicar).
      */
     public static void multiplicadorArrayList(ArrayList<Integer> numeros, int multiplicador) {
-        numeros.replaceAll(n -> n * 3);
+        // La lambda toma cada número (n) y lo reemplaza por el resultado de n * multiplicador.
+        numeros.replaceAll(n -> n * multiplicador);
         System.out.println("=== MULTIPLICADOR ===");
         System.out.println(numeros);
     }
@@ -27,6 +28,7 @@ public class EjerciciosStaticLambdas {
      * @param n (longitud de las palabras que se desean eliminar).
      */
     public static void filtroSelectivo(ArrayList<String> cadena, char letra, int n) {
+        // La lambda actúa como condición, es decir, si devuelve true, el elemento se eliminará.
         cadena.removeIf(c -> c.startsWith(String.valueOf(letra)) || c.length() < n);
         System.out.println("=== FILTRO SELECTIVO ===");
         System.out.println(cadena);
@@ -39,8 +41,12 @@ public class EjerciciosStaticLambdas {
      * @return ArrayList con las cadenas de texto convertidas a mayúscula.
      */
     public static ArrayList<String> conversorMayusculas(ArrayList<String> cadena2) {
+        // Como se pide que regrese el ArrayList, se usa return en este caso,
+        // abriendo de paso el flujo.
        return cadena2.stream()
+                 // Con map hace que cada String pase a su versión en mayúsculas.
                 .map(String::toUpperCase)
+                 // El resultado de esta transformación se almacena en un nuevo ArrayList.
                 .collect(Collectors.toCollection(ArrayList::new));
 
     }
@@ -53,8 +59,11 @@ public class EjerciciosStaticLambdas {
      */
     public static HashSet<Integer> obtenerCuadradosUnicos(ArrayList<Integer> numeros2) {
         return numeros2.stream()
+                // Dentro del filter, se condiciona a que solo pasen números pares.
                 .filter(n -> n % 2 == 0)
+                // Estos números seleccionados son elevados al cuadrado.
                 .map(n -> n * n)
+                // La colección se convierte en un Set (para eliminar duplicados).
                 .collect(Collectors.toCollection(HashSet::new));
     }
 
@@ -66,11 +75,18 @@ public class EjerciciosStaticLambdas {
      */
     public static HashMap<String, Integer> generarMapaLongitudes(ArrayList<String> palabras) {
         return palabras.stream()
+                // Dentro del toMap, se reciben dos lambdas.
                 .collect(Collectors.toMap(
+                        // La primer lambda se encarga de definir a la clave.
                         p -> p,
-                        String::length,
+                        // La segunda lambda se encarga de definir el valor.
+                        p -> p.length(),
+                        // Se añade un tercer argumento para evitar problemas con duplicados.
+                        // Si hay dos o más palabras iguales, se toma el valor de la primera
+                        // y el resto se quedarán con ese valor que ya existe.
                         (valorExistente, nuevoValor) -> valorExistente,
                         HashMap::new
+                        // Los resultados terminan guardados dentro de un HashMap.
                 ));
     }
 
@@ -80,9 +96,12 @@ public class EjerciciosStaticLambdas {
      * @param inventario (HashMap que contiene como clave: producto y valor: precio)
      */
     public static void modificadorInventario(HashMap<String, Double> inventario) {
+        // Uso de BiConsumer, que consiste en (producto, precio) -> acción.
         inventario.forEach((producto, precio) -> {
+            // Se realiza el cálculo y aplicación del descuento para cada producto en la colección.
             double descuentoAplicado = precio * 0.1;
             double precioConDescuento = precio - descuentoAplicado;
+            // Se imprime de una vez el listado del inventario en 3 partes divididas.
             System.out.printf("Producto: %s, Precio: %.2f, Precio con descuento: %.2f%n",
                     producto, precio, precioConDescuento);
         });
@@ -97,7 +116,10 @@ public class EjerciciosStaticLambdas {
      */
     public static HashMap<String, Integer> contadorFrecuencias(ArrayList<String> palabras2) {
         HashMap<String, Integer> mapaFrecuencias = new HashMap<>();
+
+        // Se recorre el ArrayList de palabras.
         for (String palabra : palabras2) {
+            // merge recibe: clave, valorInicial y una Lambda que realiza la suma de frecuencia respectiva.
             mapaFrecuencias.merge(palabra, 1, (valorActual, nuevoValor) ->
                     valorActual + nuevoValor);
         }
@@ -111,10 +133,15 @@ public class EjerciciosStaticLambdas {
      * @return solo los nombres de las palabras en un ArrayList.
      */
     public static ArrayList<String> clasificadorPalabras(HashMap<String, Integer> mapaFrecuencias, int frecuencia) {
+        // Se obtiene el set de entradas (clave, valor)
         return mapaFrecuencias.entrySet()
+                // Se crea el flujo.
                 .stream()
+                // Filtra por el valor (la frecuencia a través de una lambda).
                 .filter(m -> m.getValue() >= frecuencia)
+                // Con .map, se saca la clave (la palabra) a través de otra lambda.
                 .map(m -> m.getKey())
+                // Se guarda el resultado en un ArrayList.
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
@@ -126,11 +153,16 @@ public class EjerciciosStaticLambdas {
      * @return HashSet con todas las palabras sin duplicar y que hayan cumplido la longitud.
      */
     public static HashSet<String> deduplicacionPalabras(String frase, int longitud) {
+        // Se divide la frase en un array (usando espacios como separador).
         String[] palabras = frase.split(" ");
 
+        // Este array de palabras se pasa a un flujo.
         return Arrays.stream(palabras)
+                // Se filtran las palabras con longitud mínima con ayuda de una lambda.
                 .filter(p -> p.length() >= longitud)
+                // Con .map todas las palabras pasan a convertirse en minúsculas.
                 .map(String::toLowerCase)
+                // Todas las palabras que pasaron se almacenan en un HashSet (para evitar duplicados).
                 .collect(Collectors.toCollection(HashSet::new));
     }
 
@@ -141,10 +173,14 @@ public class EjerciciosStaticLambdas {
      * @param N (es el tope al que la frecuencia puede llegar).
      */
     public static void topeFrecuencias(HashMap<String, Integer> mapaFrecuencias2, int N) {
+        // replaceAll recibe clave y valor y actualiza el valor con el resultado de la lambda.
         mapaFrecuencias2.replaceAll((palabra, frecuencia) -> {
+            // La condición es para comprobar si la frecuencia supera el tope (N).
             if (frecuencia > N) {
+                // Si lo supera, devolverá el valor tope (N).
                 return N;
             } else {
+                // En caso contrario, conservará su valor original.
                 return frecuencia;
             }
         });
